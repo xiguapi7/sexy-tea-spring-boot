0728567d8a731f31d71645ba5a65471948580f4a
大大大西西瓜皮🍉 <adanz1015@outlook.com>
2020-11-08 15:52:24 +0800

    删除测试类。

--- sexy-tea/src/test/java/sexy/tea/ApplicationTest.java
+++ sexy-tea/src/test/java/sexy/tea/ApplicationTest.java
@@ -1,17 +1,8 @@
 package sexy.tea;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import sexy.tea.service.OrderService;
 
 @SpringBootTest
 class ApplicationTest {
 
-    @Autowired
-    private OrderService service;
-
-    @Test
-    void testCreateOrder() {
-    }
 }
